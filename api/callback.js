c8fbe10413767169610aa5ebc8ae54470802c45f
SAIG <sreearunachalainfragroup@gmail.com>
2025-11-28 18:27:33 +0530

    fix to callback page is not sending the token back to Decap CMS.

--- api/callback.js
+++ api/callback.js
@@ -1,69 +1,44 @@
-// api/callback.js
 import fetch from "node-fetch";
 
-async function exchangeCodeForToken(code, redirectUri) {
+async function getToken(code, redirectUri) {
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
     code,
-    redirect_uri: redirectUri
+    redirect_uri: redirectUri,
   });
 
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { Accept: "application/json" },
-    body: params
+    body: params,
   });
 
   return response.json();
 }
 
 export default async function handler(req, res) {
-  try {
-    const code = req.query.code;
-    const SERVER_URL = process.env.SERVER_URL;
+  const code = req.query.code;
+  const SERVER_URL = process.env.SERVER_URL;
 
-    if (!code) return res.status(400).send("Missing code");
+  const redirectUri = `${SERVER_URL}/callback`;
 
-    const redirectUri = `${SERVER_URL}/callback`;
+  const tokenData = await getToken(code, redirectUri);
 
-    const tokenResponse = await exchangeCodeForToken(code, redirectUri);
-
-    if (tokenResponse.error || !tokenResponse.access_token) {
-      console.error("OAuth Error:", tokenResponse);
-      return res.status(500).send("OAuth token exchange failed");
-    }
-
-    const accessToken = tokenResponse.access_token;
-
-    const html = `
+  const html = `
 <!doctype html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>OAuth Complete</title>
-  </head>
   <body>
-    <h3>Login Successful. You can close this window.</h3>
     <script>
-      (function() {
-        const token = "${accessToken}";
-        if (window.opener && window.opener.CMSOAuthCallback) {
-          window.opener.CMSOAuthCallback({ token: token });
-          window.close();
-        } else {
-          document.body.innerHTML =
-            "<p>Token received. Please close this window manually.</p>";
-        }
-      })();
+      window.opener.postMessage(
+        JSON.stringify({ token: "${tokenData.access_token}" }),
+        window.location.origin
+      );
+      window.close();
     </script>
   </body>
 </html>`;
 
-    res.setHeader("Content-Type", "text/html");
-    return res.status(200).send(html);
-  } catch (err) {
-    console.error("Callback Error:", err);
-    return res.status(500).send("Server error");
-  }
+  res.setHeader("Content-Type", "text/html");
+  res.send(html);
 }
