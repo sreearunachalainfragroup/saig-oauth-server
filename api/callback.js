c669fe49bee95d69852247976c3e821009589c74
SAIG <sreearunachalainfragroup@gmail.com>
2025-11-30 15:48:55 +0530

    Added token variable and try catch blocks to check errors

--- api/callback.js
+++ api/callback.js
@@ -14,16 +14,27 @@ async function getToken(code, redirectUri) {
     body: params,
   });
 
-  return response.json();
+  const data = await response.json();
+
+  if (!data.access_token) {
+    throw new Error(`GitHub token not received: ${JSON.stringify(data)}`);
+  }
+
+  return data.access_token;
 }
 
 export default async function handler(req, res) {
+  try {
+    
   const code = req.query.code;
   const SERVER_URL = process.env.SERVER_URL;
 
   const redirectUri = `${SERVER_URL}/callback`;
 
+  // Fetch the access token from GitHub
   const tokenData = await getToken(code, redirectUri);
+  // Fetch the token
+  const token = tokenData.access_token;
 
   const html = `
 <!doctype html>
@@ -46,4 +57,9 @@ export default async function handler(req, res) {
 
   res.setHeader("Content-Type", "text/html");
   res.send(html);
+
+  } catch {
+    console.error("OAuth callback error:----callback.js-->", err);
+    res.status(500).send("Internal Server Error");
+  }
 }
